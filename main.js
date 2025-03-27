37c2d1fc903bf68e19ce60f9f16bfbb47dfcb8cf
Roman Egorov <hihihi.ru1@mail.ru>
2025-03-27 16:35:28 +0300

    main

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-let buttton = document.querySelector('button');
+let buttton = document.querySelector('.plus_btn');
+let button_reset = document.querySelector('.reset_btn');
 let counterText = document.querySelector('.counter');
 let counter = 0;
 
 buttton.addEventListener('click', function(){
     counter = counter + 1;
-    
+
     counterText.innerText = counter;
+})
+
+button_reset.addEventListener('click', function(){
+    counterText.innerText = 0;
 })
 
 //DOM - Document Object Model
